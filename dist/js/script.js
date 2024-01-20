4cbcda3a528f0f0f6ef7dacdee5cf9f30da16877
Artem Tumanov home <artem.tumanov.93@inbox.ru>
2024-01-20 17:44:08 +0300

    add partners

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,13 @@
-const swiper = new Swiper('.promo__slider', {
+const promoSwiper = new Swiper('.promo__slider', {
     // Optional parameters
     direction: 'vertical',
     loop: true,
     autoplay: {
         delay: 2500,
-        disableOnInteraction: false,
+        disableOnInteraction: true,
     },
     speed: 1000,
+    effect: 'coverflow',
   
     // If we need pagination
     pagination: {
@@ -17,11 +18,28 @@ const swiper = new Swiper('.promo__slider', {
 
 const lightbox = GLightbox({ loop: true, });
 
-const buttonGrid = document.querySelector('.arrow__right')
+const buttonGrid = document.querySelector('.photo__button')
   	  firstSlide = document.querySelector('.photo__grid-first')
 	  secondSlide = document.querySelector('.photo__grid-second');
 
       buttonGrid.addEventListener('click', () => {
 		firstSlide.classList.toggle('photo__grid-first-show')
 		secondSlide.classList.toggle('photo__grid-second-hidden')
-	  })
+	  });
+
+const productSwiper = new Swiper(".partners__slider", {
+  slidesPerView: 5,
+  grid: {
+    rows: 3,
+  },
+  mousewheel: true,
+  spaceBetween: 24,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
